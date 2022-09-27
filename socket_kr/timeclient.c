f7782b60f96bc473dd2737eb0447b593434fde84
R4smuS-jpg <r4smusjpg@gmail.com>
2022-09-27 23:52:25 +0300

    code style fix

--- socket_kr/timeclient.c
+++ socket_kr/timeclient.c
@@ -3,7 +3,7 @@
 #include <arpa/inet.h>
 #include <string.h>
 
-int main() 
+int main()
 {
 	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in server_addr;
@@ -15,15 +15,15 @@ int main()
 	scanf("%s", &ip_address);
 
 	server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(1303);
-    server_addr.sin_addr.s_addr = inet_addr(ip_address);
+	server_addr.sin_port = htons(1303);
+	server_addr.sin_addr.s_addr = inet_addr(ip_address);
 
-    connect(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	connect(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
-    recv(server_socket, server_message, sizeof(server_message), 0);
-    printf("%s\n", server_message);
+	recv(server_socket, server_message, sizeof(server_message), 0);
+	printf("%s\n", server_message);
 
-    close(server_socket);
+	close(server_socket);
 
-    return 0;
+	return 0;
 }
